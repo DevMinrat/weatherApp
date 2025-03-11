59abd2e4420c9c26691ef57f302b153dd9f7ecdd
DevMinrat <devminrat@gmail.com>
2025-03-11 12:24:14 +0300

    feat: add repeat-password checker on js

--- src/main/webapp/resources/js/scripts.js
+++ src/main/webapp/resources/js/scripts.js
@@ -1,4 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    const regForm = document.querySelector("#registration-form");
+
+    if (regForm != null) {
+        const password = document.querySelector("#password");
+        const repeatedPassword = document.querySelector("#repeat-password");
+        const confirmPassword = document.querySelector("#confirm-password");
+
+        regForm.addEventListener("submit", (e) => {
+            if (password != null && repeatedPassword != null) {
+                if (password.value !== repeatedPassword.value) {
+                    e.preventDefault();
+                    confirmPassword.style.display = "block";
+                }
+            }
+        })
+    }
 
 });
